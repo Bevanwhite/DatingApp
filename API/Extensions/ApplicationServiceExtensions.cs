76dd4bf4f2f99f9fbd4e5bc83b00e97374516a6e
Bevanwhite <jefferybevan@gmail.com>
2024-12-01 01:26:14 +0530

    end of section 13

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -22,6 +22,7 @@ namespace API.Extensions
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.Configure<ColudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<IPhotoService, PhotoService>();
+            services.AddScoped<LogUserActivity>();
             return services;
         } 
     }
