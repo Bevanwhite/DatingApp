480ff8e4e9f5bb25f3b26c897802c9efcfe12a18
Bevanwhite <jefferybevan@gmail.com>
2025-01-23 19:47:27 +0530

    Add PresenceTracker for managing online users in SignalR

--- API/SignalR/PresenceHub.cs
+++ API/SignalR/PresenceHub.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.SignalR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
@@ -7,14 +8,31 @@ namespace DatingApp.API.SignalR
     [Authorize]
     public class PresenceHub : Hub
     {
+        private readonly PresenceTracker _tacker;
+
+        public PresenceHub(PresenceTracker tacker)
+        {
+            _tacker = tacker;
+
+        }
         public override async Task OnConnectedAsync()
         {
+            await _tacker.UserConnected(Context.User.GetUserName(), Context.ConnectionId);
             await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUserName());
+
+            var currentUsers = await _tacker.GetOnlineUsers();
+            await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
         }
 
         public override async Task OnDisconnectedAsync(Exception exception)
         {
+            await _tacker.UserConnected(Context.User.GetUserName(), Context.ConnectionId);
+
             await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUserName());
+
+            var currentUsers = await _tacker.GetOnlineUsers();
+
+            await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
 
             await base.OnDisconnectedAsync(exception);
         }
