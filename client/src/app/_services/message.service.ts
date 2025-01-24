8c95612b3900dca2976aa9cc8018a62b42a31fae
Bevanwhite <jefferybevan@gmail.com>
2025-01-24 14:32:20 +0530

    Refactor message handling in MemberDetail and MemberMessages components; add stopHubConnection method to MessageService

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -33,6 +33,12 @@ export class MessageService {
     });
   }
 
+  stopHubConnection() {
+    if (this.hubConnection) {
+      this.hubConnection.stop();
+    }
+  }
+
   getMessages(pageNumber: number, pageSize: number, container: string) {
     let params = getPaginationHeaders(pageNumber, pageSize);
     params = params.append('Container', container);
