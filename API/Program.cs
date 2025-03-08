91363d95329fc5f8701c3899a326a487e74e51b4
Bevanwhite <jefferybevan@gmail.com>
2025-03-08 22:21:01 +0530

    Add SSL mode requirement to PostgreSQL connection string

--- API/Program.cs
+++ API/Program.cs
@@ -34,7 +34,7 @@ else
     var pgHost = pgHostPort.Split(":")[0];
     var pgPort = pgHostPort.Split(":")[1];
 
-    connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+    connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SslMode=Require;";
 }
 builder.Services.AddDbContext<DataContext>(opt =>
 {
