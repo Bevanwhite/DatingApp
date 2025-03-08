9ef55c1d7726110db89fef7c78508bab6179195b
Bevanwhite <jefferybevan@gmail.com>
2025-03-08 22:36:47 +0530

    Update PostgreSQL connection string to use SslMode=VerifyFull

--- API/Program.cs
+++ API/Program.cs
@@ -34,7 +34,7 @@ else
     var pgHost = pgHostPort.Split(":")[0];
     var pgPort = pgHostPort.Split(":")[1];
 
-    connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SslMode=Require;";
+    connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SslMode=VerifyFull;";
 }
 builder.Services.AddDbContext<DataContext>(opt =>
 {
