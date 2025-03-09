a980dad656016c0c591806573a2efbbb348af5aa
Bevanwhite <jefferybevan@gmail.com>
2025-03-09 14:32:20 +0530

    Refactor PostgreSQL connection handling to use DATABASE_URL from configuration

--- API/Program.cs
+++ API/Program.cs
@@ -20,21 +20,7 @@ if (builder.Environment.IsDevelopment())
     connString = builder.Configuration.GetConnectionString("DefaultConnection");
 else
 {
-    // Use connection string provided at runtime by Heroku.
-    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-
-    // Parse connection URL to connection string for Npgsql
-    connUrl = connUrl.Replace("postgres://", string.Empty);
-    var pgUserPass = connUrl.Split("@")[0];
-    var pgHostPortDb = connUrl.Split("@")[1];
-    var pgHostPort = pgHostPortDb.Split("/")[0];
-    var pgDb = pgHostPortDb.Split("/")[1];
-    var pgUser = pgUserPass.Split(":")[0];
-    var pgPass = pgUserPass.Split(":")[1];
-    var pgHost = pgHostPort.Split(":")[0];
-    var pgPort = pgHostPort.Split(":")[1];
-
-    connString = $"Host={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+    connString = builder.Configuration.GetConnectionString("DATABASE_URL");
 }
 builder.Services.AddDbContext<DataContext>(opt =>
 {
